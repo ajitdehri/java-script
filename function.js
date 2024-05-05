3b1331b7c86bf2715fab5d6c09cc3c20ab829dd4
kumarajit <ajitdehri1997@gmail.com>
2024-05-05 23:39:53 +0530

    added rest parameter

--- function.js
+++ function.js
@@ -1,8 +1,32 @@
-function sum(num1 ,num2){
-    return num1+num2;
+// function sum(num1 ,num2){
+//     return num1+num2;
+// }
+
+
+// const result=sum(7,7);
+
+// console.log(result);
+
+
+// function multiplication(a,b){
+//     return a*b;
+// }
+
+// const array=[2,3];
+// const mul=multiplication(...array);
+// console.log(mul);
+
+
+
+
+function sum(...args){
+    let sum1=0;
+    for(let value of args){
+        sum1=sum1+value;
+    }
+
+    return sum1;
 }
 
-
-const result=sum(7,7);
-
+let result=sum(1,2,3,4);
 console.log(result);
