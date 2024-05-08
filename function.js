c9546bb1bfc56a0e1a1f3dffb5d770299a1ec1cb
kumarajit <ajitdehri1999@gmail.com>
2024-05-08 16:06:47 +0530

    added array

--- function.js
+++ function.js
@@ -70,8 +70,87 @@
 // Array push/pop
 
 
-const num=[1,2,3,4];
-const result=num.pop();
-num.push(7);
-// console.log(result);
+// const num=[1,2,3,4];
+// // const result=num.pop();
+// const num1=num.shift();
+// console.log(num1);
+// // console.log(result);
+// console.log(num);
+// num.unshift(9);
+// console.log(num);
+
+
+// delete method
+
+// const arr=[1,2,3,4];
+// // delete arr[2];
+// // console.log(arr);
+
+
+// //splice() method
+// arr.splice(2,0,8,9);
+
+// // arr.splice(0,2);
+// console.log(arr);
+
+
+//slice method
+
+// const num=[1,2,3,4,5];
+// const num1=num.slice(1,4);
+// console.log(num1);
+// console.log(num);
+
+
+
+//toSpliced() method
+
+// const num=[1,2,3,4];
+
+// const newNum=num.toSpliced(2,0,8,9);
+
+// console.log(newNum);
+// console.log(num);
+
+
+//concatenation--join two array
+
+
+// const arr1=[1,2,3,4,5];
+// const arr2=[6,7,8,9];
+
+// const newArr=arr2.concat(arr1);
+// console.log(newArr);
+
+
+// flat-methood
+
+
+// const a=[[1,2],[3,4],[5,6]];
+// const flattedArray=a.flat();
+// console.log(flattedArray);
+
+
+
+// sort Array---string
+
+
+// const arr=["apple","mango","orange","babana"];
+// // arr.sort();
+// // console.log(arr);
+// // arr.reverse();
+// // console.log(arr);
+
+// const newArrya=arr.toSorted();
+// console.log(arr);
+// console.log(newArrya);
+
+
+// sort Array---number
+
+const num=[8,5,3,9,2,1,122];
+num.sort(function(a,b){return b-a});
 console.log(num);
+num.reverse();
+console.log(num);
+
